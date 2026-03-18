305537a15e4d26323bc2debad6a23f3e86c5fcb9
geethika-15 <geethikaallada@gmail.com>
2026-03-18 20:47:24 +0530

    added HelloApp.java

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -4,7 +4,7 @@ public class HelloApp {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello WORLD");
         } else {
             for(int i = 0; i < args.length; i++) {
                 System.out.println("Hello " + args[i]);
